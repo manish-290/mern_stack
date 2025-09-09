1059aa6f20e0bb7c100907bea730a51b8a2147fc
Manish <manishpaudel290@gmail.com>
2025-09-09 23:44:42 +0545

    Finished Profile Card: added Show/Hide Info feature

---
+++ day1_profile/practice.js
@@ -0,0 +1,7 @@
+let name = "Manish";
+let rev;
+
+for(var i=name.length;i>0;i--){
+    rev = name[name.length]
+}
+console.log(rev)
--- day1_profile/script.js
+++ day1_profile/script.js
@@ -3,6 +3,10 @@ const bioEl = document.querySelector('.bio');
 const toggleBtn = document.getElementById('toggle-theme');
 const sayHiBtn = document.getElementById('say-hi');
 
+const card = document.querySelector('.card');
+const toggleInfoBtn = document.getElementById('toggle-info');
+const extraInfo = document.getElementById('extra-info');
+
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
@@ -11,3 +15,16 @@ sayHiBtn.addEventListener('click', () => {
   alert(`Hello, ${nameEl.textContent}! 👋\nKeep going — you're on day 1.`);
   bioEl.textContent = 'Nice click! Keep experimenting with the code.';
 });
+
+
+toggleInfoBtn.addEventListener('click',()=>{
+    extraInfo.classList.toggle('hidden');
+
+    //change button text based on state
+    if(extraInfo.classList.contains('hidden')){
+        toggleInfoBtn.textContent = "Show-info";
+    }else{
+        toggleInfoBtn.textContent = "Hide-Info";
+    }
+});
+
